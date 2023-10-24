6ef4dad2863a532ea6fef65fd342f0092b159393
Konrad Hyży <konrad.hyzy@botify.com>
2023-10-24 16:44:40 +0200

    feat: add botx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { observer } from 'mobx-react-lite';
 import { counterStore } from './utils/Store';
-import Component1 from './Folder1/Component1';
-import Component2 from './Folder1/Component2';
-import Component3 from './Folder1/Component3';
-import Component4 from './Folder1/Component4';
 
 const App = () => {
   return (
@@ -13,10 +9,7 @@ const App = () => {
       </header>
       <button onClick={() => counterStore.increaseCounter()}>ADD</button>
       <button onClick={() => counterStore.decreaseCounter()}>SUBTRACT</button>
-      <Component1 />
-      <Component2 />
-      <Component3 />
-      <Component4 />
+      <p>{counterStore.counter}</p>
     </div>
   );
 };
---
+++ src/botx/botx.js
@@ -0,0 +1,61 @@
+import { useState, useRef } from 'react';
+
+const useForceUpdate = () => {
+  const [, set] = useState(0);
+  return () => set((e) => !e);
+};
+
+let observableKeysByAction = [];
+let reactionsByKey = {};
+
+const createReaction = (onChange) => {
+  return {
+    track: (notifyObservers) => {
+      observableKeysByAction = [];
+      notifyObservers();
+      observableKeysByAction.forEach((key) => {
+        reactionsByKey[key] = reactionsByKey[key] || [];
+        reactionsByKey[key].push(onChange);
+      });
+    },
+  };
+};
+
+const observable = (obj) => {
+  return new Proxy(obj, {
+    get: (target, key) => {
+      observableKeysByAction.push(key);
+      return target[key];
+    },
+    set: (target, key, newValue) => {
+      target[key] = newValue;
+      if (reactionsByKey[key]) {
+        reactionsByKey[key].forEach((func) => func());
+      }
+
+      return true;
+    },
+  });
+};
+
+const autorun = (callBack) => {
+  let reaction = createReaction(callBack);
+  reaction.track(callBack);
+};
+
+const makeObservable = (BaseComponent) => {
+  return () => {
+    const forceUpdate = useForceUpdate();
+    const reactionRef = useRef(null);
+    if (!reactionRef.current) {
+      reactionRef.current = createReaction(forceUpdate);
+    }
+    let output;
+    reactionRef.current.track(() => {
+      output = BaseComponent();
+    });
+    return output;
+  };
+};
+
+export { autorun, observable, makeObservable };
